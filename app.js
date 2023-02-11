66427ef4ec37209b9cab3a8d03a98dbbe6f3cd87
Gourav Gupta <gouravgupta7799@gmail.com>
2023-02-11 17:08:48 +0530

    Showed added data on UI

--- app.js
+++ app.js
@@ -1,35 +1,45 @@
+let url = "https://crudcrud.com/api/000620fb92734c63b0deaeac9cbcbf92";
 
-let btn = document.getElementById('submitbtn');
+let list = document.getElementById('items');
 
-obj = {
-  name: '',
-  email: '',
-  number: ''
-}
-btn.addEventListener('click', (e) => {
+showData();
 
-  let Name = document.getElementById('NameInput');
-  let Email = document.getElementById('EmailInput');
-  let Number = document.getElementById('contactInput');
-
-  obj.name = Name.value;
-  obj.email = Email.value;
-  obj.number = Number.value;
-
-  axios.post('https://crudcrud.com/api/65cc70f8032a4c8c8d18547a4470a874/userdata', obj)
-
-    .then((response) => showData(response))
-    .catch((error) => console.log(error));
-});
-
-function showData(Response) {
-  let list = document.getElementById('items');
+function createListItem(name, email, number) {
   let li = document.createElement('li');
-  let deletebtn = document.createElement('button');
+  let deletebtn = document.createElement('Button');
   let Editbtn = document.createElement('Button');
   li.appendChild(deletebtn);
   li.appendChild(Editbtn);
-  data = Response.data
-  li.innerHTML = `${data.name} - ${data.email} - ${data.number}`
-  list.appendChild(li);
+  li.innerHTML = `${name} - ${email} - ${number}`
+  return li;
 }
+
+function showData() {
+  axios.get(url + '/userdata')
+    .then((response) => {
+      for (let i = 0; i < response.data.length; i++) {
+        list.appendChild(createListItem(response.data[i].name, response.data[i].email, response.data[i].number));
+      }
+    })
+    .catch((error) => console.log(error));
+}
+
+
+let btn = document.getElementById('submitbtn');
+btn.addEventListener('click', (e) => {
+  let obj = {
+    name: '',
+    email: '',
+    number: ''
+  }
+
+  obj.name = document.getElementById('NameInput').value;
+  obj.email = document.getElementById('EmailInput').value;
+  obj.number = document.getElementById('contactInput').value;
+
+  axios.post(url + '/userdata', obj)
+    .then(response => {
+      list.appendChild(createListItem(response.data.name, response.data.email, response.data.number));
+    })
+    .catch((error) => console.log(error));
+});
